5b45b72c8be43a886329b0234386068b9249bb7e
moai-kun <shida6295@gmail.com>
2023-08-10 17:35:54 +0900

    plan page試作2

--- plan/script.js
+++ plan/script.js
@@ -1,10 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     let w_width = window.innerWidth;
-    let w_height = window.innerHeight;
+    let w_height = window.innerHeight - 10;
     console.log(w_width, w_height)
     targetElement = document.getElementById("plan-all-container");
     targetElement.style.width = w_width + 'px'
-    targetElement.style.height = w_height + 'px'
+    targetElement.style.height = w_height - 60 + 'px' // headerとfooter分ひく
 
 });
+
+
+function click_plan(element) {
+    let parent = element.parentElement
+    // console.log(parent.id)
+    var now = new Date();
+
+
+    var Year = now.getFullYear();
+    var Month = now.getMonth() + 1;
+    var nDate = now.getDate();
+    var nHour = now.getHours();
+    var nMin = now.getMinutes();
+    console.log(nHour, nMin)
+
+
+    // 子要素をめくる
+    let child_1 = element.children[0]
+    let child_2 = element.children[1]
+    // console.log(child_1);
+    child_1.style.transform = "rotateX(" + 90 + "deg)";
+    child_2.style.transform = "rotateX(" + 0 + "deg)";
+}
